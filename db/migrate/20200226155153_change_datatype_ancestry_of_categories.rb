5a5744ae2de0a37f7e64ae5c954a412ad9996db1
saatoko <rm-as.l2j@ezweb.ne.jp>
2020-02-27 12:55:54 +0900

    カラムの型変更の記述ミス修正（change_column :table_name, :column_name, 'integer USING CAST(column_name AS integer)'）

--- db/migrate/20200226155153_change_datatype_ancestry_of_categories.rb
+++ db/migrate/20200226155153_change_datatype_ancestry_of_categories.rb
@@ -3,7 +3,7 @@ class ChangeDatatypeAncestryOfCategories < ActiveRecord::Migration[5.0]
     # 環境ごとにマイグレーションを分ける
     if Rails.env.production?
       # 本番環境はusingオプションを追加
-      change_column :categories, :ancestry, 'integer USING CAST(column_name AS integer)'
+      change_column :categories, :ancestry, 'integer USING CAST(ancestry AS integer)'
     else
       change_column :categories, :ancestry, :integer
     end
