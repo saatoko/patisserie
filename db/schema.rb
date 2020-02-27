6aafe3caaa2041af19faf3c211ab21f48eca718a
saatoko <rm-as.l2j@ezweb.ne.jp>
2020-02-27 22:02:26 +0900

    削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200226155153) do
+ActiveRecord::Schema.define(version: 20200209164843) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "ancestry"
+    t.string   "ancestry"
     t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
   end
 
